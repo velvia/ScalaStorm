b37a85d349039bfb5d70d6060360d18305e1818d
Mark Hamstra <markhamstra@gmail.com>
2012-02-13 13:53:27 -0800

    cleanup imports

--- dsl/StormBolt.scala
+++ dsl/StormBolt.scala
@@ -4,7 +4,7 @@ package storm.scala.dsl
 
 import backtype.storm.topology.base.BaseRichBolt
 import backtype.storm.topology.OutputFieldsDeclarer
-import backtype.storm.tuple.{Fields, Tuple, Values}
+import backtype.storm.tuple.{Fields, Tuple}
 import backtype.storm.task.OutputCollector
 import backtype.storm.task.TopologyContext
 import collection.JavaConversions._
@@ -23,9 +23,9 @@ import java.util.Map
 abstract class StormBolt(val outputFields: List[String]) extends BaseRichBolt with SetupFunc {
     var _collector:OutputCollector = _
     var _context:TopologyContext = _
-    var _conf:java.util.Map[_, _] = _
+    var _conf:Map[_, _] = _
 
-    def prepare(conf:java.util.Map[_, _], context:TopologyContext, collector:OutputCollector) = {
+    def prepare(conf:Map[_, _], context:TopologyContext, collector:OutputCollector) = {
         _collector = collector
         _context = context
         _conf = conf
