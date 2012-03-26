9722acc61b2a93e0fd9069c07acf1b9cded0a991
Evan Chan <ev@ooyala.com>
2012-03-26 09:25:49 -0700

    Split out bolt DSL into a trait

--- dsl/StormBolt.scala
+++ dsl/StormBolt.scala
@@ -8,6 +8,7 @@ import backtype.storm.tuple.{Fields, Tuple}
 import backtype.storm.task.OutputCollector
 import backtype.storm.task.TopologyContext
 
+
 // The StormBolt class is an implementation of IRichBolt which
 // provides a Scala DSL for making Bolt development concise.
 // To use, extend this class and implement the execute(t: Tuple) method.
@@ -17,10 +18,10 @@ import backtype.storm.task.TopologyContext
 //   anchor(<tuple>) emit (...)
 //   <tuple> emit (...)
 //   using no anchor emit (...)
-abstract class StormBolt(val streamToFields: collection.Map[String, List[String]]) extends IRichBolt with SetupFunc {
-    var _collector:OutputCollector = _
-    var _context:TopologyContext = _
-    var _conf:java.util.Map[_, _] = _
+abstract class StormBolt(val streamToFields: collection.Map[String, List[String]])
+    extends IRichBolt with SetupFunc with BoltDsl {
+    var _context: TopologyContext = _
+    var _conf: java.util.Map[_, _] = _
 
     // A constructor for the common case when you just want to output to the default stream
     def this(outputFields: List[String]) = { this(Map("default" -> outputFields)) }
@@ -39,27 +40,35 @@ abstract class StormBolt(val streamToFields: collection.Map[String, List[String]
         declarer.declareStream(stream, new Fields(fields:_*))
       }
     }
+}
 
-    // Declare an anchor for emitting a tuple
-    def anchor(tuple: Tuple) = new StormTuple(_collector, tuple)
+/**
+ *  This trait allows the bolt emit DSL to be used outside of the main bolt class, say in an Actor or
+ *  separate thread.  To use it you just need to initialise _collector before using it.
+ */
+trait BoltDsl {
+  var _collector: OutputCollector = _
 
-    def anchor(tuples: List[Tuple]) = new StormTupleList(_collector, tuples)
+  // Declare an anchor for emitting a tuple
+  def anchor(tuple: Tuple) = new StormTuple(_collector, tuple)
 
-    // Use this for unanchored emits:
-    //    using no anchor emit (val1, val2, ...)
-    def no(s: String) = new UnanchoredEmit(_collector)
+  def anchor(tuples: List[Tuple]) = new StormTupleList(_collector, tuples)
 
-    val anchor = ""
+  // Use this for unanchored emits:
+  //    using no anchor emit (val1, val2, ...)
+  def no(s: String) = new UnanchoredEmit(_collector)
 
-    // Combine with anchor for a cool DSL like this:
-    // using anchor t emit (val1, val2, ..)
-    def using = this
+  val anchor = ""
 
-    // implicitly convert to a stormTuple for easy emit syntax like
-    // tuple emit (val1, val2, ...)
-    implicit def stormTupleConvert(tuple: Tuple) =
-      new StormTuple(_collector, tuple)
+  // Combine with anchor for a cool DSL like this:
+  // using anchor t emit (val1, val2, ..)
+  def using = this
 
-    implicit def stormTupleListConverter(tuples: Seq[Tuple]) =
-      new StormTupleList(_collector, tuples)
+  // implicitly convert to a stormTuple for easy emit syntax like
+  // tuple emit (val1, val2, ...)
+  implicit def stormTupleConvert(tuple: Tuple) =
+    new StormTuple(_collector, tuple)
+
+  implicit def stormTupleListConverter(tuples: Seq[Tuple]) =
+    new StormTupleList(_collector, tuples)
 }
